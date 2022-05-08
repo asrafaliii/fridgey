02f91b58d794e84c26467236b7b0a6cb5943b322
asrafaliii <asraf.bookaand@gmail.com>
2022-05-08 21:06:47 +0600

    inastall react router and create navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blog from './components/Blog/Blog';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home/Home';
+import Login from './components/Login/Login';
+import Products from './components/Home/Products/Products';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Word</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/products" element={<Products />}/>
+        <Route path="/blog" element={<Blog />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route pthh="*" element={<NotFound />}/>
+      </Routes>
+      <Footer />
     </div>
   );
 }
